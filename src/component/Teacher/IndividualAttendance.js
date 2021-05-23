3be6d5a247f6352d69c27b449436c20bc754b3a1
samir <samir1741012321@gmail.com>
2021-05-23 20:28:45 +0530

    Attendance Module

--- src/component/Teacher/IndividualAttendance.js
+++ src/component/Teacher/IndividualAttendance.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { Table } from "react-bootstrap";
 import axios from "axios";
+import moment from "moment";
 import { URL } from "../../Constants";
 export default class IndividualAttendance extends Component {
   constructor() {
     super();
     this.state = {
       attendanceList: [],
+      presentDate: [],
     };
   }
   async componentDidMount() {
@@ -15,8 +18,10 @@ export default class IndividualAttendance extends Component {
           studentid: this.props.match.params.id,
         })
         .then((res) => {
-          console.log(res.data);
-          this.setState({ attendanceList: res.data });
+          this.setState({
+            attendanceList: res.data,
+            presentDate: res.data.map((value) => value.date),
+          });
         })
         .catch((e) => {
           console.log(e);
@@ -25,7 +30,68 @@ export default class IndividualAttendance extends Component {
     getAttendanceByTeacher();
   }
   render() {
-    // const { props } = this;
-    return <div>Hello</div>;
+    const { presentDate } = this.state;
+    const date = new Date();
+    const dateArray = Array.from({ length: moment().daysInMonth() }).map(
+      (value, index) => {
+        return {
+          date: moment(
+            `${date.getFullYear()}-${date.getMonth() + 1}-${index + 1}`,
+            "YYYY-MM-DD"
+          ).format("YYYY-MM-DD"),
+        };
+      }
+    );
+    return (
+      <div style={{ padding: "20px" }}>
+        <h3 style={{ marginBottom: "20px" }}>
+          Attendance Register for {this.props.match.params.id}
+        </h3>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <span>
+                    Date (<strong>YYYY-MM-DD</strong>)
+                  </span>
+                </div>
+              </th>
+              <th style={{ display: "flex", justifyContent: "center" }}>
+                Marked
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {dateArray.map((val, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {val.date}
+                    </div>
+                  </td>
+                  <td style={{ display: "flex", justifyContent: "center" }}>
+                    {presentDate.includes(val.date) ? "Present" : "-"}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 }
